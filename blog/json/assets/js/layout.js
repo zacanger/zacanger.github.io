f890877c0eb366af9cfc412b90e3fa332cbbaee6
zacanger <zac@zacanger.com>
2016-05-05 14:18:32 -0600

    foo

--- blog/json/assets/js/layout.js
+++ blog/json/assets/js/layout.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $('.indentation-container' ).on('click', '.indentation.indentation-toggle', function() {
-    $( this ).closest( '.indentation-container' ).addClass( 'collapsed' );
-  });
+    $(this).closest('.indentation-container').addClass('collapsed')
+  })
 
   $('.indentation-container' ).on('click', '.indentation-ellipsis.indentation-toggle', function() {
-    $( this ).closest( '.indentation-container' ).removeClass( 'collapsed' );
-  });
+    $(this).closest('.indentation-container').removeClass('collapsed')
+  })
 
-  $('[data-toggle="tooltip"]').tooltip();
-});
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
