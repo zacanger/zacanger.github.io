c6189c5c629bff03f6752a57f068c1a3c170466e
zacanger <zac@zacanger.com>
2016-06-04 15:25:01 -0600

    cv removal

--- cv/index.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-require('fs').createReadStream('./zacanger.json').pipe(process.stdout)
-
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const
 
 app
 .use(express.static(__dirname))
+.get('/cv', (req, res) => res.sendFile(__dirname + '/zacanger.json'))
 .use((req, res) => res.sendFile(__dirname + '/404/index.html'))
 .listen(port, () => console.log(`zacanger listening on ${port}`))
 
