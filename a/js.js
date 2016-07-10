b177b6bb680aee0bbf00bbef0a7e2aadd0338875
zacanger <zac@zacanger.com>
2016-07-09 18:33:35 -0600

    a

--- a/js.js
+++ a/js.js
@@ -1,60 +1,38 @@
-navigator.getUserMedia = (
-  navigator.getUserMedia       ||
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia    ||
-  navigator.msGetUserMedia
-)
+'use strict'
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-var analyser = audioCtx.createAnalyser()
-analyser.minDecibels           = -90
-analyser.maxDecibels           = -20
-analyser.smoothingTimeConstant = .89
+var Audio = window.AudioContext || window.webkitAudioContext
+var audioCtx = new Audio()
 
-var wrapper   = document.querySelector('.wrapper')
-var canvas    = document.querySelector('.visualizer')
-var canvasCtx = canvas.getContext('2d')
-canvas.setAttribute('width'  , wrapper.clientWidth)
-canvas.setAttribute('height' , wrapper.clientHight)
+var canvas = document.createElement('canvas')
 
-navigator.getUserMedia({audio : true}, visualize, console.error.bind(console))
+canvas.setAttribute('width', '1024px')
+canvas.setAttribute('height', '400px')
 
-function visualize (stream) {
-  audioCtx.createMediaStreamSource(stream).connect(analyser)
-  WIDTH = canvas.width
-  HEIGHT = canvas.height
+var ctx = canvas.getContext('2d')
+var raw = undefined
 
-  analyser.fftSize = 256
-  var bufferLength = analyser.frequencyBinCount
-  var dataArray    = new Uint8Array(bufferLength)
-  var start        = Date.now()
+document.body.appendChild(canvas)
 
-  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
-
-  draw()
-
-  function draw () {
-    drawVisual = requestAnimationFrame(draw)
-
-    analyser.getByteFrequencyData(dataArray)
-
-    canvasCtx.fillStyle = 'rgb(0, 0, 0)'
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
-
-    var barWidth = (WIDTH / bufferLength) / 1.5
-    var x        = 0
-    var diff     = -(Date.now() - start) / 30
-    var barHeight
-
-    for (var i = 1; i < bufferLength; i++) {
-      barHeight = dataArray[i]
-
-      var color = (((i / bufferLength) * 270) + diff) % 360
-
-      canvasCtx.fillStyle = 'hsla(' + -color + ', ' + Math.max(50, 100 * barHeight / 128) + '%,' + Math.min(50, barHeight / 3) + '%, 1)'
-      canvasCtx.fillRect(WIDTH / 2 + x, HEIGHT - barHeight / 2, barWidth, barHeight / 2)
-      canvasCtx.fillRect(WIDTH / 2 - x, HEIGHT - barHeight / 2, barWidth, barHeight / 2)
-      x += barWidth + 3
-    }
+;(function paintFrame () {
+  if (raw) {
+    ctx.clearRect(0, 0, 1024, 400)
+    raw.forEach(function (v, i) {
+      var n = (v + 1) / 2
+      ctx.fillRect(i, n * 400, 2, 2)
+    })
   }
-}
+
+  window.requestAnimationFrame(paintFrame)
+})()
+
+navigator.webkitGetUserMedia({ audio: true }, function (stream) {
+  var mic = audioCtx.createMediaStreamSource(stream)
+  var processor = audioCtx.createScriptProcessor(1024, 1, 1)
+
+  mic.connect(processor)
+  processor.connect(audioCtx.destination)
+
+  processor.onaudioprocess = function (e) {
+    raw = Array.prototype.slice.call(e.inputBuffer.getChannelData(0), 0)
+  }
+}, function (err) {})
