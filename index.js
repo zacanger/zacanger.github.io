0bed3bc14fdbea1c9a620d0ba6f73dfa88d97e20
zacanger <zac@zacanger.com>
2016-07-28 17:18:14 -0600

    new version of me. yay.

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const
 
 app
 .use(express.static(__dirname))
-.get('/cv', (req, res) => res.sendFile(__dirname + '/zacanger.json'))
-.use((req, res) => res.sendFile(__dirname + '/404/index.html'))
+.get('/cv', (req, res) => res.sendFile(`${__dirname}/zacanger.json`))
+.use((req, res) => res.sendFile(`${__dirname}/404/index.html`))
 .listen(port, () => console.log(`zacanger listening on ${port}`))
