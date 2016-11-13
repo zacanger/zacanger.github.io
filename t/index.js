b542f1d624cf83727f39e28ca7700faf2cfb4db4
zacanger <zac@zacanger.com>
2016-11-13 14:13:29 -0700

    converter thing

--- t/index.js
+++ t/index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
-'use strict'
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
+const { writeFile } = require('fs')
+const getData = img => img.replace(/^data:image\/\w+;base64,/, '')
+const getRandomString = () => Math.random().toString(36).substring(8)
+const getBuf = d => Buffer.from(d, 'base64')
+const writeImg = b => writeFile(`${getRandomString()}.png`, b)
 
-const
-  express = require('express')
-, port    = process.env.PORT || 3000
-, app     = express()
-
+// get img from post on go button click
+// img -> writeImg(getBuf(getData(img))) -> tweet
 app
 .disable('x-powered-by')
 .use(express.static(__dirname))
