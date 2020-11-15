0c8ff769cffa9e6e68e3e30032c5dad4d05a738b
zacanger <zac@zacanger.com>
2020-11-14 21:42:38 -0700

    chore: build

--- blog/lib/components/format-date.js
+++ blog/lib/components/format-date.js
@@ -2,6 +2,6 @@ import moment from 'moment'
 
 export default (date) => {
   const a = new Date(date)
-  const d = a.getTime() + Math.abs(a.getTimezoneOffset() * 60000)
+  const d = a.getTime() + Math.abs(a.getTimezoneOffset() * 60_000)
   return moment(d).format('DD MMMM, YYYY')
 }
