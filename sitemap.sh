f0ab784d2d17c98d73e75861525b4008d5c4bb22
zacanger <zac@zacanger.com>
2020-12-26 16:03:05 -0700

    fix: more sitemap tweaks

--- sitemap.sh
+++ sitemap.sh
@@ -3,25 +3,24 @@ set -e
 
 # Mac/BSD compatibility
 if hash gfind 2>/dev/null; then
-  _find=gfind
+  _find='gfind'
 else
-  _find=find
+  _find='find'
 fi
 if hash gsed 2>/dev/null; then
-  _sed=gsed
+  _sed='gsed'
 else
-  _sed=sed
+  _sed='sed'
 fi
 
 ROOT="https://zacanger.com/"
-
 
 # begin new sitemap
 exec 1> sitemap.xml
 
 # print head
-echo '<?xml version="1.0" encoding="UTF-8"?>'
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'
 
 # print urls
 $_find . -type f -name '*.html' -printf "%TY-%Tm-%Td%p\n" | \
@@ -38,29 +37,51 @@ $_find . -type f -name '*.html' -printf "%TY-%Tm-%Td%p\n" | \
     if [[ $file == "" ]]; then
       priority=1.0
       freq="monthly"
-    elif [[ $file == "cv" ]]; then
-      priority=0.8
+    elif [[ $file == "cv/" ]]; then
+      priority=0.9
       freq="monthly"
-    elif [[ $file == "presentations" ]]; then
+    elif [[ $file == "presentations/" ]]; then
       priority=0.4
       freq="yearly"
-    elif [[ $file == "blog" ]]; then
+    elif [[ $file == "blog/" ]]; then
       priority=0.6
       freq="daily"
     elif [[ $file == "404.html" ]]; then
-      priority=0.2
+      priority=0.0
       freq="never"
     fi
 
     uri=${ROOT}${file}
 
-    echo "<url>"
-    echo "  <loc>$uri</loc>"
-    echo "  <lastmod>$date</lastmod>"
-    echo "  <changefreq>$freq</changefreq>"
-    echo "  <priority>$priority</priority>"
-    echo "</url>"
+    echo "<url>
+  <loc>$uri</loc>
+  <lastmod>$date</lastmod>
+  <changefreq>$freq</changefreq>
+  <priority>$priority</priority>
+</url>"
   done
 
-# print foot
+# A couple of extras that show up because they're on GitHub Pages
+echo "<url>
+  <loc>https://zacanger.com/hey-you</loc>
+  <lastmod>2020-01-19</lastmod>
+  <priority>0.3</priority>
+</url>
+<url>
+  <loc>https://zacanger.com/zeess</loc>
+  <lastmod>2020-01-19</lastmod>
+  <priority>0.0</priority>
+</url>
+<url>
+  <loc>https://zacanger.com/teamethod</loc>
+  <lastmod>2020-01-19</lastmod>
+  <priority>0.1</priority>
+</url>
+<url>
+  <loc>https://zacanger.com/zeelib/</loc>
+  <lastmod>2020-01-19</lastmod>
+  <priority>0.2</priority>
+</url>"
+
+# Close it out
 echo "</urlset>"
