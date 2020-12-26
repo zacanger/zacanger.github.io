133e1f6dfbecac4b480c308726dc46ae61422229
zacanger <zac@zacanger.com>
2020-12-26 15:52:26 -0700

    fix: filter out templates as well

--- sitemap.sh
+++ sitemap.sh
@@ -25,41 +25,42 @@ echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="htt
 
 # print urls
 $_find . -type f -name '*.html' -printf "%TY-%Tm-%Td%p\n" | \
-while read -r line; do
-  date=${line:0:10}
-  file_html=${line:12}
-  file=${file_html%index.html}
+  $_sed "/blog\/templates/d" | $_sed "/presentations\/__template/d" | \
+  while read -r line; do
+    date=${line:0:10}
+    file_html=${line:12}
+    file=${file_html%index.html}
 
-  # options: always hourly daily weekly monthly yearly never
-  freq="weekly"
-  priority=0.5
+    # options: always hourly daily weekly monthly yearly never
+    freq="weekly"
+    priority=0.5
 
-  if [[ $file == "" ]]; then
-    priority=1.0
-    freq="monthly"
-  elif [[ $file == "cv" ]]; then
-    priority=0.8
-    freq="monthly"
-  elif [[ $file == "presentations" ]]; then
-    priority=0.4
-    freq="yearly"
-  elif [[ $file == "blog" ]]; then
-    priority=0.6
-    freq="daily"
-  elif [[ $file == "404.html" ]]; then
-    priority=0.2
-    freq="never"
-  fi
+    if [[ $file == "" ]]; then
+      priority=1.0
+      freq="monthly"
+    elif [[ $file == "cv" ]]; then
+      priority=0.8
+      freq="monthly"
+    elif [[ $file == "presentations" ]]; then
+      priority=0.4
+      freq="yearly"
+    elif [[ $file == "blog" ]]; then
+      priority=0.6
+      freq="daily"
+    elif [[ $file == "404.html" ]]; then
+      priority=0.2
+      freq="never"
+    fi
 
-  uri=${ROOT}${file}
+    uri=${ROOT}${file}
 
-  echo "<url>"
-  echo "  <loc>$uri</loc>"
-  echo "  <lastmod>$date</lastmod>"
-  echo "  <changefreq>$freq</changefreq>"
-  echo "  <priority>$priority</priority>"
-  echo "</url>"
-done
+    echo "<url>"
+    echo "  <loc>$uri</loc>"
+    echo "  <lastmod>$date</lastmod>"
+    echo "  <changefreq>$freq</changefreq>"
+    echo "  <priority>$priority</priority>"
+    echo "</url>"
+  done
 
 # print foot
 echo "</urlset>"
