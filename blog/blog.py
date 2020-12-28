38b034ab98fb2028d01a9e920af642934f676f4a
zacanger <zac@zacanger.com>
2020-12-27 19:50:32 -0700

    chore(blog.py): lots of cleanup

--- blog/blog.py
+++ blog/blog.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 
-
-"""
-Simple blog renderer. Only requirements are Jinja and py-gfm.
-"""
-
-
 import os
 import shutil
 import yaml
 import re
-
-# from datetime import datetime
+from datetime import datetime
 from jinja2 import Environment, PackageLoader, select_autoescape
 import markdown
 from mdx_gfm import PartialGithubFlavoredMarkdownExtension
 from feedgen.feed import FeedGenerator
 
+# TODO: This module is a binding to github's cmark fork,
+# so it includes all the stuff i want, but it's a brand new
+# project so I want to make sure it's stable before switching.
 # import pycmarkgfm
 
 
@@ -25,15 +21,11 @@ all_posts = os.listdir(src_dir)
 out_dir = "posts"
 templates_dir = "./templates"
 index = "index.html"
-index_data = []
-feed_data = []
 jinja_env = Environment(loader=PackageLoader("blog", "templates"))
 
 
 def render_md(source):
-    # TODO: this module is a binding to github's cmark fork,
-    # so it includes all the stuff i want, but it's a brand new
-    # project so i want to make sure it's stable before switching.
+    # TODO: See above.
     # return pycmarkgfm.gfm_to_html(source)
     return markdown.markdown(
         source,
@@ -41,20 +33,15 @@ def render_md(source):
     )
 
 
-def generate_index():
+def generate_index(data):
     template = jinja_env.get_template(index)
-    data = sorted(
-        index_data,
-        key=lambda x: (x["created"], x["title"]),
-        reverse=True,
-    )
     rendered = template.render({"posts": data})
     with open(index, "w") as i:
         i.write(rendered)
         i.close()
 
 
-def generate_feed():
+def generate_feed(data):
     fg = FeedGenerator()
     fg.id("https://zacanger.com/blog/")
     fg.title("Zac Anger's Blog")
@@ -62,22 +49,28 @@ def generate_feed():
     fg.link(href="https://zacanger.com/blog/")
     fg.description("Programming, socialism, and Buddhism.")
     fg.language("en")
-    for entry in index_data:
+
+    for entry in data:
         url = "https://zacanger.com" + entry["href"]
-        # published = datetime.strptime(entry["created"], "%Y-%m-%d")
+        # This causes every post to have a pubDate of midnight MST,
+        # but since I don't keep track of the actual time, it's
+        # close enough.
+        published = datetime.strptime(
+            str(entry["created"]) + "--07:00", "%Y-%m-%d-%z"
+        )
         fe = fg.add_entry()
         fe.id(url)
         fe.title(entry["title"])
         fe.link(href=url)
         fe.author({"name": "Zac Anger"})
         fe.description(entry["body"])
-        # fe.published(published)
-        # try: fe.published(entry["created"]) except: print(entry["created"])
+        fe.pubDate(published)
 
     fg.rss_file("rss.xml")
 
 
 def main():
+    index_data = []
     # First remove old posts, since sometimes posts get renamed, removed, etc.
     shutil.rmtree("./posts")
     for post in all_posts:
@@ -98,11 +91,11 @@ def main():
                 return
             metadata["href"] = "/blog/" + destination_dir
             try:
-                blog = render_md(parts[2])
+                body = render_md(parts[2])
             except IndexError:
                 print("File missing body after frontmatter:", post)
                 return
-            index_data.append({**metadata, "body": blog})
+            index_data.append({**metadata, "body": body})
             template = jinja_env.get_template("post.html")
 
             required_keys = ["title", "created", "tags"]
@@ -118,17 +111,22 @@ def main():
                         "created": metadata["created"],
                         "tags": metadata["tags"],
                         "starred": metadata.get("starred", False),
-                        "contents": blog,
+                        "body": body,
                     }
                 }
             )
+
             with open(destination_file, "w") as d:
                 d.write(rendered)
                 d.close()
 
-            # These depend on having appended all metadata above
-            generate_index()
-            generate_feed()
+            index_data.sort(
+                key=lambda x: (x["created"], x["title"]),
+                reverse=True,
+            )
+
+            generate_index(index_data)
+            generate_feed(list(reversed(index_data)))
 
 
 if __name__ == "__main__":
