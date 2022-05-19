8b20de1b1457839e321efb05522649f85eedff2b
zacanger <zac@zacanger.com>
2022-05-19 12:55:10 -0600

    chore: add blog linting code

--- blog/blog.py
+++ blog/blog.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+"""
+Tiny blog generator. See constants below for settings.
+"""
+
 import os
+import sys
 import shutil
-import yaml
-import re
 from datetime import datetime
-from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+import yaml
 import markdown
+from jinja2 import Environment, FileSystemLoader
 from mdx_gfm import PartialGithubFlavoredMarkdownExtension
 from feedgen.feed import FeedGenerator
 
@@ -16,19 +21,19 @@ from feedgen.feed import FeedGenerator
 # import pycmarkgfm
 
 # settings
-my_name = "Zac Anger"
-blog_title = my_name + "'s Blog"
-site_root = "https://zacanger.com"
-blog_root = site_root + "/blog/"
-src_dir = "./src"
-all_posts = os.listdir(src_dir)
-out_dir = "posts"
-templates_dir = "./templates"
-index = "index.html"
-jinja_env = Environment(loader=FileSystemLoader("templates"))
+MY_NAME = "Zac Anger"
+BLOG_TITLE = MY_NAME + "'s Blog"
+SITE_ROOT = "https://zacanger.com"
+BLOG_ROOT = SITE_ROOT + "/blog/"
+SRC_DIR = "./src"
+ALL_POSTS = os.listdir(SRC_DIR)
+OUT_DIR = "posts"
+INDEX = "index.html"
+JINJA_ENV = Environment(loader=FileSystemLoader("templates"))
 
 
 def render_md(source):
+    "Renders markdown to HTML"
     # TODO: See above.
     # return pycmarkgfm.gfm_to_html(source)
     return markdown.markdown(
@@ -38,48 +43,51 @@ def render_md(source):
 
 
 def generate_index(data):
-    template = jinja_env.get_template(index)
+    "Generates index.html"
+    template = JINJA_ENV.get_template(INDEX)
     rendered = template.render({"posts": data})
-    with open(index, "w") as i:
+    with open(INDEX, "w") as i:
         i.write(rendered)
         i.close()
 
 
 def generate_feed(data):
-    fg = FeedGenerator()
-    fg.id(blog_root)
-    fg.title(blog_title)
-    fg.author({"name": my_name})
-    fg.link(href=blog_root)
-    fg.description("Programming, socialism, and Buddhism.")
-    fg.language("en")
+    "Generates the RSS feed"
+    feed_generator = FeedGenerator()
+    feed_generator.id(BLOG_ROOT)
+    feed_generator.title(BLOG_TITLE)
+    feed_generator.author({"name": MY_NAME})
+    feed_generator.link(href=BLOG_ROOT)
+    feed_generator.description("Programming, socialism, and Buddhism.")
+    feed_generator.language("en")
 
     for entry in data:
-        url = site_root + entry["href"]
+        url = SITE_ROOT + entry["href"]
         # This causes every post to have a pubDate of midnight MST,
         # but since I don't keep track of the actual time, it's
         # close enough.
         published = datetime.strptime(
             str(entry["created"]) + "--07:00", "%Y-%m-%d-%z"
         )
-        fe = fg.add_entry()
-        fe.id(url)
-        fe.title(entry["title"])
-        fe.link(href=url)
-        fe.author({"name": my_name})
-        fe.description(entry["body"])
-        fe.pubDate(published)
+        feed_entry = feed_generator.add_entry()
+        feed_entry.id(url)
+        feed_entry.title(entry["title"])
+        feed_entry.link(href=url)
+        feed_entry.author({"name": MY_NAME})
+        feed_entry.description(entry["body"])
+        feed_entry.pubDate(published)
 
-    fg.rss_file("rss.xml")
+    feed_generator.rss_file("rss.xml")
 
 
 def process_post(post):
-    destination_dir = out_dir + "/" + post.replace(".md", "")
-    destination_file = destination_dir + "/" + index
+    "Reads a post source and processes it into an HTML file"
+    destination_dir = OUT_DIR + "/" + post.replace(".md", "")
+    destination_file = destination_dir + "/" + INDEX
     os.makedirs(destination_dir)
 
-    with open(src_dir + "/" + post) as p:
-        contents = p.read()
+    with open(SRC_DIR + "/" + post) as source_post:
+        contents = source_post.read()
         # Split out frontmatter. This will return an empty string, the
         # frontmatter yaml, and then the post body which may have more
         # occurrences of ---.
@@ -88,20 +96,20 @@ def process_post(post):
             metadata = yaml.safe_load(parts[1])
         except IndexError:
             print("File missing frontmatter:", post)
-            return
+            sys.exit(1)
         metadata["href"] = "/blog/" + destination_dir
         try:
             body = render_md(parts[2])
         except IndexError:
             print("File missing body after frontmatter:", post)
-            return
-        template = jinja_env.get_template("post.html")
+            sys.exit(1)
+        template = JINJA_ENV.get_template("post.html")
 
         required_keys = ["title", "created", "tags"]
         for k in required_keys:
             if k not in metadata:
                 print("Key", k, "not in file", post)
-                return
+                sys.exit(1)
 
         rendered = template.render(
             {
@@ -115,19 +123,20 @@ def process_post(post):
             }
         )
 
-        with open(destination_file, "w") as d:
-            d.write(rendered)
-            d.close()
+        with open(destination_file, "w") as dest_file:
+            dest_file.write(rendered)
+            dest_file.close()
 
     # returned for use in generating index and rss
     return {**metadata, "body": body}
 
 
 def main():
+    "Runs the whole thing"
     index_data = []
     # First remove old posts, since sometimes posts get renamed, removed, etc.
     shutil.rmtree("./posts")
-    for post in all_posts:
+    for post in ALL_POSTS:
         metadata = process_post(post)
         index_data.append(metadata)
 
---
+++ blog/lint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+black -l 79 blog.py
+pylint blog.py
